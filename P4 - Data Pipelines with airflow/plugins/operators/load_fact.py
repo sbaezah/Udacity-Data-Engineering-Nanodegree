73b8fef3c9d0b4466617d21e8a2bd7a4652adba1
Sebastian Baeza <seba.baeza@gmail.com>
2019-10-21 00:02:38 -0300

    Add files via upload
    Load Fact tables operator script.

---
+++ P4 - Data Pipelines with airflow/plugins/operators/load_fact.py
@@ -0,0 +1,42 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+    
+    """
+        This script loads data into the fact table
+        
+        Input parameters
+            redshift_conn_id: the name of the connection that should be configured in airflow, previously of the DAG execution.
+            
+            destination_table: name of the destination table to insert/update data. This table should be created previously of the DAG execution.
+            
+            sql_statement: Is a 'select' query that gets data from staging tables and load the extracted data into the destination table (fact table).
+        
+        
+        Output: Nothing.
+    """
+  
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id = "",
+                 destination_table = "",
+                 sql_statement = "",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.destination_table=destination_table
+        self.sql_statement=sql_statement
+
+    def execute(self, context):
+        self.log.info('Fetching redshift hook')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info('Starting: Load fact table')
+        insert_query = 'INSERT INTO {} ({})'.format(self.destination_table, self.sql_statement)
+        redshift.run(insert_query)
+        self.log.info('Finished: Load fact table')
